bbbe696a6c892f449eb2779d40e57b39e95a563a
AayushiGhag <73219902+AayushiGhag@users.noreply.github.com>
2025-01-06 18:15:48 +0530

    Update cis_level1_server-20250104.0404.sh

--- cis_level1_server-20250104.0404.sh
+++ cis_level1_server-20250104.0404.sh
@@ -504,51 +504,51 @@ fi
 ###############################################################################
 # BEGIN fix (25 / 277) for 'accounts_password_pam_dcredit'
 ###############################################################################
-(>&2 echo "Remediating rule 25/277: 'accounts_password_pam_dcredit'")
-# Remediation is applicable only in certain platforms
-if dpkg-query --show --showformat='${db:Status-Status}\n' 'libpam-runtime' 2>/dev/null | grep -q installed; then
+# (>&2 echo "Remediating rule 25/277: 'accounts_password_pam_dcredit'")
+# # Remediation is applicable only in certain platforms
+# if dpkg-query --show --showformat='${db:Status-Status}\n' 'libpam-runtime' 2>/dev/null | grep -q installed; then
 
 
-var_password_pam_dcredit='-1'
+# var_password_pam_dcredit='-1'
 
 
 
-# Test if the config_file is a symbolic link. If so, use --follow-symlinks with sed.
-# Otherwise, regular sed command will do.
-sed_command=('sed' '-i')
-if test -L "/etc/security/pwquality.conf"; then
-    sed_command+=('--follow-symlinks')
-fi
+# # Test if the config_file is a symbolic link. If so, use --follow-symlinks with sed.
+# # Otherwise, regular sed command will do.
+# sed_command=('sed' '-i')
+# if test -L "/etc/security/pwquality.conf"; then
+#     sed_command+=('--follow-symlinks')
+# fi
 
-# If the cce arg is empty, CCE is not assigned.
-if [ -z "" ]; then
-    cce="CCE"
-else
-    cce=""
-fi
+# # If the cce arg is empty, CCE is not assigned.
+# if [ -z "" ]; then
+#     cce="CCE"
+# else
+#     cce=""
+# fi
 
-# Strip any search characters in the key arg so that the key can be replaced without
-# adding any search characters to the config file.
-stripped_key=$(sed 's/[\^=\$,;+]*//g' <<< "^dcredit")
+# # Strip any search characters in the key arg so that the key can be replaced without
+# # adding any search characters to the config file.
+# stripped_key=$(sed 's/[\^=\$,;+]*//g' <<< "^dcredit")
 
-# shellcheck disable=SC2059
-printf -v formatted_output "%s = %s" "$stripped_key" "$var_password_pam_dcredit"
+# # shellcheck disable=SC2059
+# printf -v formatted_output "%s = %s" "$stripped_key" "$var_password_pam_dcredit"
 
-# If the key exists, change it. Otherwise, add it to the config_file.
-# We search for the key string followed by a word boundary (matched by \>),
-# so if we search for 'setting', 'setting2' won't match.
-if LC_ALL=C grep -q -m 1 -i -e "^dcredit\\>" "/etc/security/pwquality.conf"; then
-    "${sed_command[@]}" "s/^dcredit\\>.*/$formatted_output/gi" "/etc/security/pwquality.conf"
-else
-    # \n is precaution for case where file ends without trailing newline
-    printf '\n# Per %s: Set %s in %s\n' "$cce" "$formatted_output" "/etc/security/pwquality.conf" >> "/etc/security/pwquality.conf"
-    printf '%s\n' "$formatted_output" >> "/etc/security/pwquality.conf"
-fi
+# # If the key exists, change it. Otherwise, add it to the config_file.
+# # We search for the key string followed by a word boundary (matched by \>),
+# # so if we search for 'setting', 'setting2' won't match.
+# if LC_ALL=C grep -q -m 1 -i -e "^dcredit\\>" "/etc/security/pwquality.conf"; then
+#     "${sed_command[@]}" "s/^dcredit\\>.*/$formatted_output/gi" "/etc/security/pwquality.conf"
+# else
+#     # \n is precaution for case where file ends without trailing newline
+#     printf '\n# Per %s: Set %s in %s\n' "$cce" "$formatted_output" "/etc/security/pwquality.conf" >> "/etc/security/pwquality.conf"
+#     printf '%s\n' "$formatted_output" >> "/etc/security/pwquality.conf"
+# fi
 
-else
-    >&2 echo 'Remediation is not applicable, nothing was done'
-fi
-# END fix for 'accounts_password_pam_dcredit'
+# else
+#     >&2 echo 'Remediation is not applicable, nothing was done'
+# fi
+# # END fix for 'accounts_password_pam_dcredit'
 
 ###############################################################################
 # BEGIN fix (26 / 277) for 'accounts_password_pam_lcredit'
